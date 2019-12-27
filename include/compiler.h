b5937d9e182beecfe4eb7a8e511f002abd9033a6
Patryk Obara <dreamer.tan@gmail.com>
2019-12-27 20:37:57 +0100

    Test for C++20

--- include/compiler.h
+++ include/compiler.h
@@ -19,22 +19,56 @@
 #ifndef DOSBOX_COMPILER_H
 #define DOSBOX_COMPILER_H
 
+// fallback for old compilers missing attribute queries
+// 
+#ifndef __has_cpp_attribute
+#  define __has_cpp_attribute(x) 0
+#endif
+
 /* 
  * Old DOSBox names GCC_LIKELY/GCC_UNLIKELY are not precise, since this
  * feature is supported by other compilers as well (e.g. Clang, Intel).
  * '!!' trick is used to assure macro semantics match user expectations.
  */
 
+// FIXME this is how it's supposed to work according to spec, but
+// it does not work in GCC 9 yet (feature seems incomplete, even with
+// -std=c++2a
+
+// if LIKELY (a > 0) {
+// 	…
+// }
+
+/*
+
+#if __has_cpp_attribute(likely)
+#  define LIKELY(x) (x) [[likely]]
+#elif C_HAS_BUILTIN_EXPECT
+#  define LIKELY(x) (__builtin_expect(!!(x), 1))
+#else
+#  define LIKELY
+#endif
+
+#if __has_cpp_attribute(unlikely)
+#  define UNLIKELY(x) (x) [[unlikely]]
+#elif C_HAS_BUILTIN_EXPECT
+#  define UNLIKELY(x) (__builtin_expect(!!(x), 0))
+#else
+#  define UNLIKELY
+#endif
+
+*/
+
+// if (GCC_LIKELY(a > 0)) {
+// 	…
+// }
+
 #if C_HAS_BUILTIN_EXPECT
-# define LIKELY(x)       __builtin_expect(!!(x), 1)
-# define UNLIKELY(x)     __builtin_expect(!!(x), 0)
 # define GCC_LIKELY(x)   __builtin_expect(!!(x), 1)
 # define GCC_UNLIKELY(x) __builtin_expect(!!(x), 0)
 #else
-# define LIKELY       (x) (x)
-# define UNLIKELY     (x) (x)
-# define GCC_LIKELY   (x) (x)
-# define GCC_UNLIKELY (x) (x)
+# define GCC_LIKELY
+# define GCC_UNLIKELY
 #endif
 
 #endif /* DOSBOX_COMPILER_H */
